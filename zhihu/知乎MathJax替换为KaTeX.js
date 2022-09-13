d0e6f02c569bdadf288f0e59485bf6d17b4a05ca
Mirion Lee <mirion@qq.com>
2022-09-13 14:06:03 +0800

    Update 知乎MathJax替换为KaTeX.js

--- zhihu/知乎MathJax替换为KaTeX.js
+++ zhihu/知乎MathJax替换为KaTeX.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         知乎MathJax替换为KaTeX
-// @version      0.9.1
+// @version      0.9.2
 // @author       Mirion
 // @match        https://zhuanlan.zhihu.com/*
 // @require      https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.js
@@ -14,65 +14,105 @@
 	'use strict';
 
 	let options = {
+		// 自动区分行内、行间公式（不区分的话katex将使用default配置，区分的话行内、行间公式分别使用inline,display配置，配置参数详见https://katex.org/docs/options.html）
 		autoDistinguish: true,
 		default: {
 			displayMode: false,
-			css: ''
+			macros: {}
 		},
 		inline: {
 			displayMode: true,
-			css: 'display: inline-block; margin: 0;'
+			macros: {}
 		},
 		display: {
 			displayMode: true,
-			css: ''
+			macros: {}
 		}
 	}
 
-	GM_addStyle(GM_getResourceText('KATEX_CSS')
-		.replaceAll('fonts/', 'https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/fonts/')
-		.replaceAll('.eqn-num', '$') // 制裁自动编号
-	)
+	// 修正css样式
+	GM_addStyle(GM_getResourceText('KATEX_CSS').replaceAll('fonts/', 'https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/fonts/'))
+	GM_addStyle(`
+		.katex-display>.katex { /* 行间公式自动换行 */
+			white-space: normal;
+		}
+
+		.eqn-num { /* 隐藏自动编号 */
+			display: none !important;
+		}
+
+		[inline-formula] .katex-display,
+		[inline-formula] .katex-display>.katex,
+		[inline-formula] .katex-display>.katex>.katex-html {
+			display: inline;
+		}
+	`)
+
+	// 修正katex中缺失的命令
+	katex.renderToString(String.raw `
+		\def\label#1{}
+	`, {
+		globalGroup: true,
+		macros: options.default.macros
+	})
+	options.inline.macros = options.display.macros = options.default.macros
 
 	document.querySelectorAll('.ztext-math').forEach(el => {
 		try {
-			let latex = normalize(el.getAttribute('data-tex'))
-			let op = options.default
+			let latex = normalize(el.dataset.tex)
+			let opt = options.default
 			if (options.autoDistinguish) {
 				el.parentElement.childNodes.forEach(i => { // 清除空结点，方便判定
 					if (i.nodeType === 3 && i.data.trim() === '') {
 						i.remove()
 					}
 				})
-				op = isDisplayFormula(el, latex) ? options.display : options.inline
+				if (isDisplayFormula(el, latex)) {
+					opt = options.display
+					el.setAttribute('display-formula', '')
+				} else {
+					opt = options.inline
+					el.setAttribute('inline-formula', '')
+				}
 			}
-            console.log(katex)
-			katex.render(latex, el, {
-				displayMode: op.displayMode
-			})
-			//el.firstChild.removeAttribute('class') // 清除katex的样式
-			el.firstChild.style = op.css
+			katex.render(latex, el, opt)
 		} catch (err) {
 			el.innerHTML = '<code style="color: red;">' + err + '</code>'
 		}
 	})
 
-	function normalize(s) {
-		// 修正MathJax中正常但会使得KaTeX报错的latex代码
-		return (s.trim() + ' ') // 兼容末尾单独的\
-			.replace(/\\begin{array}\s*(\[.*?\])?\s*&/g, '\\begin{array}c') // 兼容\begin{array}&...
-			.replace(/\\color{\s*/g, '\\color{') // 兼容\color{<空格>#...}
-			.replace(/\\bbox\s*(\[.*?\])?/g, '\\boxed') // 兼容\bbox...
+	function normalize(str) {
+		// 去除多余空白字符
+		str = ' ' + str.replace(/\s+/g, ' ') + ' '
+		let t = ''
+		for (let i = 1; i < str.length - 1; ++i) {
+			if (/\s/.test(str[i])) {
+				if (str[i - 1] === '\\' || /[a-zA-Z]/.test(str[i - 1]) && /[a-zA-Z]/.test(str[i + 1])) {
+					t += str[i]
+				} else {
+					continue
+				}
+			} else {
+				t += str[i]
+			}
+		}
+		// 修正mathjax中正常但会使katex报错的latex代码
+		return t
+			.replace(/\\begin{array}(\[.*?\])?&/g, '\\begin{array}c') // 兼容\begin{array}&
+			.replace(/\\bbox(\[.*?\])?/g, '\\boxed') // 兼容\bbox（退化成\boxed）
 	}
 
-	function isDisplayFormula(e, latex) {
-		if (/(\\begin{(equation|align|gather|alignat|CD|darray|dcases|drcases)}|\\(tag))/.test(latex)) { // 检测通常用在行间公式中的命令
+	function isDisplayFormula(el, latex) {
+		if (el.parentElement.tagName !== 'P' && el.parentElement.childElementCount === 1) { // 非p元素的唯一子元素
+			return false
+		}
+		if (/(\\begin{(equation|align|aligned|gather|alignat|CD|darray|dcases|drcases)}|\\(tag))/.test(latex)) { // 检测通常用在行间公式中的命令
 			return true
 		}
-		if(latex.endsWith('\\\\ ')){ // 知乎上通常以这种方式督促公式居中，所以判定为行间公式
+		if (latex.endsWith('\\\\ ')) { // 知乎上通常以这种方式督促公式居中，所以判定为行间公式
 			return true
 		}
-		if (isEmpty(e.previousSibling) && isEmpty(e.nextSibling)) { // 两侧为空
+		if (isEmpty(el.previousSibling) && isEmpty(el.nextSibling)) { // 两侧为空
 			return true
 		}
 		return false
