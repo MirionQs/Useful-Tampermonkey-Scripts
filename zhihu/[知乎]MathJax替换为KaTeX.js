06c15676ad8d5c9c849258d5668c2437e35896db
Mirion Lee <mirion@qq.com>
2022-10-12 09:24:22 +0800

    Update [知乎]MathJax替换为KaTeX.js

--- zhihu/[知乎]MathJax替换为KaTeX.js
+++ zhihu/[知乎]MathJax替换为KaTeX.js
@@ -18,6 +18,7 @@
 
 	GM_addStyle(GM_getResourceText('KATEX_CSS').replaceAll('fonts/', 'https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/fonts/'))
 
+	// 预定义宏
 	let macros = {}
 	katex.renderToString(String.raw `
 		\def\label#1{}
@@ -39,12 +40,22 @@
 	function renderByKatex(parent) {
 		parent.querySelectorAll('.ztext-math').forEach(el => el.className = 'math-formula') // 阻止MathJax
 		parent.querySelectorAll('.math-formula').forEach(el => {
+			el.parentElement.childNodes.forEach(i => { // 清除空节点
+				if (i.nodeType === 3 && i.data.trim() === '') {
+					i.remove()
+				}
+			})
 			let latex = normalize(el.dataset.tex)
 			try {
 				el.innerHTML = katex.renderToString(latex, {
 					macros: macros
 				})
 				if (isDisplayFormula(el, latex)) {
+					[el.previousSibling, el.nextSibling].forEach(i => { // 清除两侧换行
+						if (i !== null && i.nodeType === 1 && i.tagName === 'BR') {
+							i.remove()
+						}
+					})
 					el.style = 'display: block; text-align: center;'
 				}
 			} catch (e) {
@@ -55,28 +66,27 @@
 
 	function normalize(latex) {
 		return ('\\displaystyle ' + latex + ' ')
-			.replace(/(?<!\\)\s+(?![a-zA-Z\s])|(?<![a-zA-Z\\])\s+(?=[a-zA-Z])/g, '') // 清除多余的空白
-			.replace(/\\begin{array}(\[.*?\])?&/g, '\\begin{array}c') // 兼容\begin{array}&
+			.replace(/(?<!\\)\s+(?![a-zA-Z\s])|(?<![a-zA-Z\\])\s+(?=[a-zA-Z])/g, '') // 清除多余空白
+			.replaceAll('{align}', '{aligned}') // align只能用于display模式
+			.replace(/{array}(\[.*?\])?&/g, '{array}c') // 兼容\begin{array}&
 	}
 
 	function isDisplayFormula(el, latex) {
 		if (latex.endsWith('\\\\ ')) { // 知乎上的行间公式写法
 			return true
 		}
-		if (/(\\begin{(equation|align|aligned|gather|alignat|CD|darray|dcases|drcases)}|\\(tag))/.test(latex)) { // 检测通常出现在行间公式中的命令
+		if (/({(equation|align|aligned|gather|alignat|CD|darray|dcases|drcases)}|\\(tag))/.test(latex)) { // 检测通常出现在行间公式中的命令
 			return true
 		}
 		if (el.parentElement.tagName !== 'P' && el.parentElement.childElementCount === 1) { // 非p元素的唯一子元素
 			return false
 		}
-		if (isEmpty(el.previousSibling) && isEmpty(el.nextSibling)) { // 两侧为空
+		if ([el.previousSibling, el.nextSibling].every(i =>
+				i === null ||
+				i.nodeType === 1 && (i.tagName === 'BR' || i.className === 'math-formula')
+			)) { // 两侧为空
 			return true
 		}
 		return false
-
-		function isEmpty(e) {
-			return e === null ||
-				e.nodeType === 1 && (e.tagName === 'BR' || e.className === 'math-formula')
-		}
 	}
 })()
